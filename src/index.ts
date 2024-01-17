99b3e1fe2fa5028fd29f1e0937eb4dad485e27df
Ulyana <lana.arfinova@gmail.com>
2024-01-17 19:29:30 +0300

    added css

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log(4);
+export interface toDoCalendar {
+    createToDo: string;
+    getToDos: string;
+    updateToDo: string;
+    deleteToDo: string;
+}
+
+export function filterByDate() {
+
+}
+
+export function filterByRealization() {
+
+}
