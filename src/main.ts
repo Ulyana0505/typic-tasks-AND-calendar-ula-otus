2eb5190edadbb3fed25ddd43d65e592cb9efbb26
Ulyana <lana.arfinova@gmail.com>
2024-01-17 15:11:24 +0300

    all tasks except 1 are done

--- src/main.ts
+++ src/main.ts
@@ -8,33 +8,107 @@
 // console.log(hof(3, 4)(5, 6)(7)); // 25
 // console.log(hof(4, 5)(6)(7, 8)); // 30
 // console.log(hof(5)(6)(7)(8)(9)); // 35
+export function func(a: number, b: number, c?: number, d?: number, e?: number) {
+    let C = c || 0;
+    let D = d || 0;
+    let E = e || 0;
+    return a + b + C + D + E;
+    // if(c) {
+    //     return a + b + c;
+    // } else if(c && d) {
+    //     return a + b + c + d;
+    // } else if(c && d && e) {
+    //     return a + b + c + d + e;
+    // } else {
+    //     return a + b;
+    // }
+}
+export function curry(f, a?, b?, c?, d?, e?) {
+    // return function(a: number) {
+    //     return function(b: number) {
+    //         return f(a, b);
+    //     }
+    // }
+
+    return function(a: number) {
+        return function(b: number) {
+            return function(c: number) {
+                return function(d: number){
+                    return function(e: number){
+                        return f(a, b, c, d, e);
+                    }
+                }
+            }
+        }
+    }
+
+    // if(c) {
+    //     return a + b + c;
+    // } else if(c && d) {
+    //     return a + b + c + d;
+    // } else if(c && d && e) {
+    //     return a + b + c + d + e;
+    // } else {
+    //     return a + b;
+    // }
+}
+export const hof = curry(func);
+
+
 
 
 // ---2---
-// Написать функцию сумматор. При вызове функции с аргументами она суммирует переданные значения (полезно прочитать про методы .valueOf и .toString)
-// const sum = function() { /* put your code here */};
-// alert(sum()); // 0;
-// const s = sum();
-// alert(s(1)); // 1
-// alert(s(1)(2)); //3
-// alert(s(3)(4)(5)); // 12
-// const s3 = sum(3);
-// alert(s3(5)); // 8
-// alert(s3(6)); // 9
+// Написать функцию сумматор. При вызове функции с аргументами она суммирует переданные значения
+export const sum = function (x?: number) {
+    let acc = x || 0;
+
+    function summarise(y: number) {
+        acc += y;
+        return summarise;
+    }
+
+    summarise.toString = function() {
+        return acc;
+    }
+
+    return summarise;
+}
 
 
 // ---3---
-// Реализовать функцию параллельной потоковой обработки данных. В конструктор передается число парралельных "потоков", которое указывает сколько данных обрабатывается в конкретный момент времени
-// const runner = new Parallel(2);
-// console.log(await runner
-// .jobs(
-// () => new Promise((resolve) => setTimeout(resolve, 10, 1)),
-// () => new Promise((resolve) => setTimeout(resolve, 50, 2)),
-// () => new Promise((resolve) => setTimeout(resolve, 20, 3)),
-// () => new Promise((resolve) => setTimeout(resolve, 90, 4)),
-// () => new Promise((resolve) => setTimeout(resolve, 30, 5)),
-// )) // [1, 3, 2, 4, 5];
+// Реализовать функцию параллельной потоковой обработки данных. В конструктор передается число парралельных "потоков", 
+// которое указывает сколько данных обрабатывается в конкретный момент времени
+type Job<T> = () => Promise<T>
 
+export class Parallel {
+    constructor(private limit: number) {
+    }
+
+    async jobs<T>(...jobList: Job<T>[]) {
+
+        const results = [] as T[];
+
+        const started = new Map<number, Promise<void>>();
+        let position = 0;
+        while (position < jobList.length || started.size) {
+            if (started.size < this.limit && position < jobList.length) {
+                const current = position;
+                started.set(current, jobList[current]().then(result => {
+                    results.push(result);
+                    started.delete(current);
+                }));
+                position += 1;
+            }
+            await timer();
+        }
+
+        return results
+    }
+}
+
+function timer() {
+    return new Promise(r => setTimeout(r, 0))
+}
 
 // ---4---
 // Реализовать фукнцию, возвращающую развернутую по спирали матрицу (любой размерности)
@@ -46,7 +120,67 @@
 // ]); // [0, 1, 2, 3, 4, 9, 14, 19, 18, 17, 16, 15, 10, 5, 6, 7, 8, 13, 12, 11]
 
 
+// моя провальная попыточка
+// export function spiral(arr: number[][]) {
+//     const reverse = (arr: number[][]) => arr.map((line) => line.reverse());
+//     const rearrange = (arr: number[][]) => {
+//         const rows = arr.length;
+//         const cols = arr[0].length;
+//         const result: number [][] = [];
+//         for(let i = 0; i < rows; i++) {
+//             result[i] = [];
+//             for(let j = 0; j < cols; j++){
+//                 result[i][j] = arr[j][i];
+//             }
+//         }
+//         return result;
+//     }
+
+//     const sliced = arr.slice(1);
+//     const reversed = reverse(sliced);
+//     const rearranged = rearrange(reversed);
+//     const spiraled = spiral(rearranged);
+//     return spiraled;
+// };
+
+// взятый по той ссылке код, только поставила создание констант transpose и reverse также внутрь функции
+//все работает и ПОЧТИ все понятно. Плыву только в transpose, особенно в цикле for 
+export function spiral(arr: number[][]): number[] {
+    const transpose = (arr: number[][]) => {
+        const rows = arr.length;
+        const cols = arr[0] !== undefined ? arr[0].length : 0;
+        const result: number[][] = [];
+        for (let i = 0; i < cols; ++i) {
+          result[i] = [];
+          for (let j = 0; j < rows; ++j) {
+            result[i][j] = arr[j][i];
+          }
+        }
+        return result;
+    };
+
+    const reverse = (arr: number[][]) => arr.map((row) => row.reverse());
+
+    if (arr.length === 0) {
+      return [];
+    }
+  
+    const sliced = arr.slice(1);
+    const reversed = reverse(sliced);
+    const transposed = transpose(reversed);
+    const spiraled = spiral(transposed);
+    return arr[0].concat(spiraled);
+}
+
 // ---5---
 // Реализовать функцию, реализующую сортировку с учетом правил semver
-// semverSort([ "1.0.5", "2.5.0", "0.12.0", "1", "1.23.45", "1.4.50", "1.2.3.4.5.6.7"]); // [ "0.12.0", "1", "1.0.5", "1.2.3.4.5.6.7", "1.4.50", "1.23.45", "2.5.0" ]
-
+export function semverSort(arr: string[]) {
+    const biggerArr: string[] = arr.map((el) =>
+        el.split(".").map((num) => +num + 100).join(".")
+    );
+    const sortedBiggerArr: string[] = biggerArr.sort();
+    const sortedArr: string[] = sortedBiggerArr.map((el) =>
+        el.split(".").map((num) => +num - 100).join(".")
+    );
+    return sortedArr;
+};
---
+++ tests/main.test.ts
@@ -0,0 +1,75 @@
+import { func, curry, hof, sum, Parallel, spiral, semverSort } from "../src/main";
+
+// ---1---
+describe("test of curry", () => {
+    it("expect hof to return right number", () => {
+        // expect(+hof(1, 2, 3, 4, 5)).toEqual(15);
+        // expect(+hof(2, 3, 4)(5, 6)).toEqual(20);
+        // expect(+hof(3, 4)(5, 6)(7)).toEqual(25);
+        // expect(+hof(4, 5)(6)(7, 8)).toEqual(30);
+        expect(+hof(5)(6)(7)(8)(9)).toEqual(35);
+    });
+});
+// Пример использования функции
+// const func = (a, b, c, d, e) => a + b + c + d + e;
+// const hof = yourFunction(func);
+// console.log(hof(1, 2, 3, 4, 5)); // 15
+// console.log(hof(2, 3, 4)(5, 6)); // 20
+// console.log(hof(3, 4)(5, 6)(7)); // 25
+// console.log(hof(4, 5)(6)(7, 8)); // 30
+// console.log(hof(5)(6)(7)(8)(9)); // 35
+
+// ---2---
+describe("test of sum", () => {
+    it("results of sum()", () => {
+        expect(+sum()).toEqual(0);
+    });
+    it("results if const s = sum()", () => {
+        const s0 = sum();
+        expect(+s0(1)).toEqual(1);
+        const s1 = sum();
+        expect(+s1(1)(2)).toEqual(3);
+        const s2 = sum();
+        expect(+s2(3)(4)(5)).toEqual(12);
+    });
+    it("results if const s = sum(3)", () => {
+        const s3 = sum(3);
+        expect(+s3(5)).toEqual(8);
+        const s4 = sum(3);
+        expect(+s4(6)).toEqual(9);
+    });
+});
+
+// ---3---
+describe("parallel", () => {
+    it("jobs", async ()=>{
+        const runner = new Parallel(2);
+        expect(await runner.jobs(
+            () => new Promise((resolve) => setTimeout(resolve, 10, 1)),
+            () => new Promise((resolve) => setTimeout(resolve, 50, 2)),
+            () => new Promise((resolve) => setTimeout(resolve, 20, 3)),
+            () => new Promise((resolve) => setTimeout(resolve, 90, 4)),
+            () => new Promise((resolve) => setTimeout(resolve, 30, 5)),
+        )).toEqual([1, 3, 2, 5, 4]);
+    });
+});
+
+// ---4---
+describe("test of spiral", () => {
+    it("should return right spiraled arr", () => {
+        expect(spiral([[0, 1, 2, 3, 4], [5, 6, 7, 8, 9], [10, 11, 12, 13, 14], [15, 16, 17, 18, 19]])).toEqual([0, 1, 2, 3, 4, 9, 14, 19, 18, 17, 16, 15, 10, 5, 6, 7, 8, 13, 12, 11]);
+    })
+})
+// spiral([
+// [0, 1, 2, 3, 4],
+// [5, 6, 7, 8, 9],
+// [10, 11, 12, 13, 14],
+// [15, 16, 17, 18, 19]
+// ]); // [0, 1, 2, 3, 4, 9, 14, 19, 18, 17, 16, 15, 10, 5, 6, 7, 8, 13, 12, 11]
+
+// ---5---
+describe("test of semverSort", () => {
+    it("should return right sorted arr", () => {
+        expect(semverSort(["1.0.5", "2.5.0", "0.12.0", "1", "1.23.45", "1.4.50", "1.2.3.4.5.6.7"])).toEqual(["0.12.0", "1", "1.0.5", "1.2.3.4.5.6.7", "1.4.50", "1.23.45", "2.5.0"]);
+    })
+});
