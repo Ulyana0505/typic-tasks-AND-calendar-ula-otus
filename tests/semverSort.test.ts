d48205d4852cd962e6e5038ec25cdb8c2649b5cd
Ulyana <lana.arfinova@gmail.com>
2024-01-26 18:58:41 +0300

    semverSort done

--- tests/semverSort.test.ts
+++ tests/semverSort.test.ts
@@ -2,7 +2,7 @@ import { semverSort } from '../src/semverSort';
 
 // ---5---
 describe('test of semverSort', () => {
-    it('should return right sorted arr', () => {
+    it('should return right sorted arr with 7 strings', () => {
         expect(
             semverSort([
                 '1.0.5',
@@ -22,5 +22,45 @@ describe('test of semverSort', () => {
             '1.23.45',
             '2.5.0',
         ])
-    })
+    });
+    it('should return right sorted arr with 5 strings', () => {
+        expect(
+            semverSort([
+                '1.5.0',
+                '0.12.0',
+                '1',
+                '1.23.45',
+                '1.4.50',
+            ])
+        ).toEqual([
+            '0.12.0',
+            '1',
+            '1.4.50',
+            '1.5.0',
+            '1.23.45',
+        ])
+    });
+    it('should return right sorted arr with 8 strings', () => {
+        expect(
+            semverSort([
+                '1.5.0',
+                '0.12.0',
+                '1',
+                '1.23.45',
+                '1.4.50',
+                '0.3.0',
+                '1.21.2',
+                '1.1.1',
+            ])
+        ).toEqual([
+            '0.3.0',
+            '0.12.0',
+            '1',
+            '1.1.1',
+            '1.4.50',
+            '1.5.0',
+            '1.21.2',
+            '1.23.45',
+        ])
+    });
 })
