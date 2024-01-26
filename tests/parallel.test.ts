bfae194ab47b0a92db49322938faf79013a66792
Ulyana <lana.arfinova@gmail.com>
2024-01-26 18:54:25 +0300

    parallel done

--- tests/parallel.test.ts
+++ tests/parallel.test.ts
@@ -2,7 +2,7 @@ import { Parallel } from '../src/parallel';
 
 // ---3---
 describe('parallel', () => {
-    it('jobs', async () => {
+    it('jobs, Parallet take two', async () => {
         const runner = new Parallel(2)
         expect(
             await runner.jobs(
@@ -13,5 +13,17 @@ describe('parallel', () => {
                 () => new Promise((resolve) => setTimeout(resolve, 30, 5))
             )
         ).toEqual([1, 3, 2, 5, 4])
+    })
+    it('jobs, Parallet take three', async () => {
+        const runner = new Parallel(3)
+        expect(
+            await runner.jobs(
+                () => new Promise((resolve) => setTimeout(resolve, 90, 1)),
+                () => new Promise((resolve) => setTimeout(resolve, 60, 2)),
+                () => new Promise((resolve) => setTimeout(resolve, 20, 3)),
+                () => new Promise((resolve) => setTimeout(resolve, 10, 4)),
+                () => new Promise((resolve) => setTimeout(resolve, 30, 5))
+            )
+        ).toEqual([3, 4, 2, 5, 1])
     })
 })
